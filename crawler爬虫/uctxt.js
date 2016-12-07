f04dadecafe131b6e8c2625a72e89786b6d84be5
fan.jinlong@qq.com <fan.jinlong@qq.com>
2016-12-07 14:14:17 +0800

    a

--- crawler爬虫/uctxt.js
+++ crawler爬虫/uctxt.js
@@ -12,17 +12,19 @@ var ep = new eventproxy(),
 // function start() {
 	function onRequest(request, response) {
 		pageURLs.forEach(function (pageURL) {
-			superagent.get(pageURL).end(function (error, pres) {
+			superagent.get(pageURL)
+			.end(function (error, pres) {
+				if (error) {return next(error)}
 				// pres.text 里面存储着请求返回的 html 内容，将它传给 cheerio.load 之后
               // 就可以得到一个实现了 jquery 接口的变量，我们习惯性地将它命名为 `$`
               // 剩下就都是利用$ 使用 jquery 的语法了
               var $ = cheerio.load(pres.text)
               var title = $('.BookTitle')
-              console.log(title)
+              
 			})
 		})
 		response.write("<br/>")
-		response.write(pres.text)
+		response.write()
 
 	}
 
